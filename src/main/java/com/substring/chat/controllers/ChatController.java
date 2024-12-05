be75288902dcbbcde6d709dcfe52034960514ace
Lokesh kumar Raiger <168194176+lokeshKumarRaiger@users.noreply.github.com>
2024-12-06 00:35:03 +0530

    Add files via upload

---
+++ src/main/java/com/substring/chat/controllers/ChatController.java
@@ -0,0 +1,21 @@
+package com.substring.chat.controllers;
+
+import com.substring.chat.entities.Message;
+import com.substring.chat.entities.Room;
+import com.substring.chat.playload.MessageRequest;
+import com.substring.chat.repositories.RoomRepository;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.time.LocalDateTime;
+
+@Controller
+@CrossOrigin("http://localhost:5173")
+public class ChatController {
+
+
+}
