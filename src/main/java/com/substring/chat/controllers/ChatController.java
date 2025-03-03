a183db0a34405a8ff49ecb23200e31876fdb9364
Lokesh kumar Raiger <168194176+lokeshKumarRaiger@users.noreply.github.com>
2025-03-03 11:37:25 +0530

    Update ChatController.java

--- src/main/java/com/substring/chat/controllers/ChatController.java
+++ src/main/java/com/substring/chat/controllers/ChatController.java
@@ -18,4 +18,35 @@ import java.time.LocalDateTime;
 public class ChatController {
 
 
+    private RoomRepository roomRepository;
+
+    public ChatController(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+
+    //for sending and receiving messages
+    @MessageMapping("/sendMessage/{roomId}")// /app/sendMessage/roomId
+    @SendTo("/topic/room/{roomId}")//subscribe
+    public Message sendMessage(
+            @DestinationVariable String roomId,
+            @RequestBody MessageRequest request
+    ) {
+
+        Room room = roomRepository.findByRoomId(request.getRoomId());
+        Message message = new Message();
+        message.setContent(request.getContent());
+        message.setSender(request.getSender());
+        message.setTimeStamp(LocalDateTime.now());
+        if (room != null) {
+            room.getMessages().add(message);
+            roomRepository.save(room);
+        } else {
+            throw new RuntimeException("room not found !!");
+        }
+
+        return message;
+
+
+    }
 }
